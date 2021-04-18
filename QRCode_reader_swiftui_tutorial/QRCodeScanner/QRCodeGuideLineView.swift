1e9ed8fc0744801f9f53b9586a3f90a3281eac8e
Jeff, Jeong  정 의정 <35421421+TuenTuenna@users.noreply.github.com>
2021-04-18 17:24:12 +0900

    지오메트리 가이드 라인 부분 포지션 수정

--- QRCode_reader_swiftui_tutorial/QRCodeScanner/QRCodeGuideLineView.swift
+++ QRCode_reader_swiftui_tutorial/QRCodeScanner/QRCodeGuideLineView.swift
@@ -13,6 +13,9 @@ struct QRCodeGuideLineView : View {
         GeometryReader{ geometryProxy in
             RoundedRectangle(cornerRadius: 20).stroke(style: StrokeStyle(lineWidth: 10, dash: [11]))
                 .frame(width: geometryProxy.size.width / 2, height: geometryProxy.size.height / 3)
+                // geometry 프록시로 포지션을 주시면 되요!
+                // 이부분 추가해주십쇼!
+                .position(x: geometryProxy.size.width / 2, y: geometryProxy.size.height / 2)
                 .foregroundColor(Color.yellow)
         }
     }
